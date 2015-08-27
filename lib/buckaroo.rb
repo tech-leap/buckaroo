9b433d6b8761e21bbd29b1c6d6f03d9310c0957f
Nickolas <zenkovnick@gmail.com>
2015-08-27 09:30:12 +0300

    Visa and MasterCard as requested services

--- lib/buckaroo.rb
+++ lib/buckaroo.rb
@@ -52,7 +52,7 @@ module Buckaroo
       request = {}
 
       request['brq_currency'] = 'EUR'
-      request['brq_requestedservices'] = 'ideal,transfer'
+      request['brq_requestedservices'] = 'visa,mastercard'
       request['brq_culture'] = 'nl-NL'
       request['brq_continue_on_incomplete'] = 'RedirectToHTML'
 
